9c5ff0d824c1b9e5e333ed1a2ae942f9207cd705
Jason Poovey <jason.poovey@gtri.gatech.edu>
2014-11-06 15:01:22 -0500

    Live updates

--- js/controllers/StingerViz.js
+++ js/controllers/StingerViz.js
@@ -1,4 +1,42 @@
 angular.module("StingerWidgetsApp",['angular-treemap']).controller('StingerViz',function($scope,$compile,$http,$window,$timeout) {
+    function getStingerData(alg, metric, size, wait) {
+
+        var args = {"jsonrpc":"2.0", "method":"get_data_array_sorted_range", "params":{"name":alg,"data":metric,
+            "strings":true,"offset":0,"wait_for_update":wait,"count":size,"order":"DESC"}, "id":1};
+        var config = {
+            headers: {
+                "Content-Type":"application/x-www-form-urlencoded"
+            }
+        };
+
+
+        var promise = $http.post('http://' + window.location.hostname + ':8088/jsonrpc',args,config)
+            .success(function(data, status, headers, config) {
+
+                var pagerankData = {
+                    "name": "Top " + $scope.algorithm.name,
+                    "children": [],
+                    "color":200
+                };
+
+                for (var i=0; i < data.result[metric].value.length; i++) {
+                    pagerankData.children.push({
+                        "name":data.result[metric].vertex_str[i],
+                        "value":data.result[metric].value[i],
+                        "color":50
+                    })
+                }
+
+                $scope.treeData = pagerankData;
+            }).error(function(data,status,headers,config) {
+                console.log('Damnit');
+            });
+
+        promise.then(function () {
+           getStingerData(alg,metric,size,true);
+        });
+    }
+
     $scope.formatName = function(name) {
         return name;
     };
@@ -13,80 +51,34 @@ angular.module("StingerWidgetsApp",['angular-treemap']).controller('StingerViz',
         { id: 2, name: 'Betweenness Centrality' }
     ];
 
-    $scope.selectedItem = null;
+    $scope.algorithm = $scope.items[0];
+
+    $scope.sizes = [
+        { id: 30, name: '30' },
+        { id: 50, name: '50' },
+        { id: 100, name: '100' }
+    ];
+
+    $scope.size = $scope.sizes[0];
 
     $scope.treeData = {};
 
-    $scope.$watch('algorithm',function(newVal) {
-        if (!newVal) {
+    $scope.$watchGroup(['algorithm','size'],function(newVals) {
+        if (!$scope.algorithm || !$scope.size) {
             return;
         }
-        if (newVal.id==1) {
-            var args = {"jsonrpc":"2.0", "method":"get_data_array_sorted_range", "params":{"name":"pagerank","data":"pagerank",
-                "strings":true,"offset":0,"wait_for_update":false,"count":30,"order":"DESC"}, "id":1};
-            var config = {
-                headers: {
-                    "Content-Type":"application/x-www-form-urlencoded"
-                }
-            };
-
-
-            $http.post('http://' + window.location.hostname + ':8088/jsonrpc',args,config)
-                .success(function(data, status, headers, config) {
-                    $scope.rawTreeData = data.result.pagerank;
-
-                    var pagerankData = {
-                        "name": "Top Pagerank",
-                        "children": [],
-                        "color":200
-                    };
-
-                    for (var i=0; i < data.result.pagerank.value.length; i++) {
-                        pagerankData.children.push({
-                            "name":data.result.pagerank.vertex_str[i],
-                            "value":data.result.pagerank.value[i],
-                            "color":50
-                        })
-                    }
-
-                    $scope.treeData = pagerankData;
-                }).error(function(data,status,headers,config) {
-                    console.log('Damnit');
-                });
-        } else if (newVal.id==2) {
-            var args = {"jsonrpc":"2.0", "method":"get_data_array_sorted_range", "params":{"name":"betweenness_centrality","data":"bc",
-                "strings":true,"offset":0,"wait_for_update":false,"count":30,"order":"DESC"}, "id":1};
-            var config = {
-                headers: {
-                    "Content-Type":"application/x-www-form-urlencoded"
-                }
-            };
-
-
-            $http.post('http://' + window.location.hostname + ':8088/jsonrpc',args,config)
-                .success(function(data, status, headers, config) {
-                    $scope.rawTreeData = data.result.bc;
-
-
-                    var betweennessData = {
-                        "name": "Top Betweenness",
-                        "children": [],
-                        "color":200
-                    };
-
-                    for (var i=0; i < data.result.bc.value.length; i++) {
-                        betweennessData.children.push({
-                            "name":data.result.bc.vertex_str[i],
-                            "value":data.result.bc.value[i],
-                            "color":50
-                        })
-                    }
-
-                    $scope.treeData = betweennessData;
-                }).error(function(data,status,headers,config) {
-                    console.log('Damnit');
-                });
+        var alg;
+        var metric;
+        if ($scope.algorithm.id==1) {
+            alg = "pagerank";
+            metric = "pagerank";
+        } else if ($scope.algorithm.id==2) {
+            alg = "betweenness_centrality";
+            metric = "bc";
         }
+
+        getStingerData(alg,metric,$scope.size.id,false);
+
     });
 
 });
